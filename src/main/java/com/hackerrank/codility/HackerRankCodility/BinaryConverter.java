dedfccdbd41fa463625b196f1abb6a3df18c3336
erencan <erencan@netas.com.tr>
2017-01-23 13:08:29 +0300

    Binary Converter

---
+++ src/main/java/com/hackerrank/codility/HackerRankCodility/BinaryConverter.java
@@ -0,0 +1,19 @@
+package com.hackerrank.codility.HackerRankCodility;
+
+
+public class BinaryConverter {
+	
+    public static  StringBuilder calculateBinaryValue(int N)
+    {
+        StringBuilder binary = new StringBuilder();
+        while(N > 1)
+        {
+            int value = N%2;
+            binary.append(value);
+            N = N / 2;
+        }
+        binary.append(N);
+         return binary.reverse();
+    }
+
+}
---
+++ src/test/java/com/hackerrank/codility/HackerRankCodility/BinaryConverterTest.java
@@ -0,0 +1,41 @@
+package com.hackerrank.codility.HackerRankCodility;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class BinaryConverterTest {
+	
+	@Test
+	public void calculateBinaryValue_DecimalNumber_BinaryRepresentation()
+	{
+		StringBuilder binaryRepresentaion = BinaryConverter.calculateBinaryValue(9);
+		assertTrue("1001".equals(binaryRepresentaion.toString()));
+		
+		binaryRepresentaion = BinaryConverter.calculateBinaryValue(1);
+		assertTrue("1".equals(binaryRepresentaion.toString()));
+		
+		binaryRepresentaion = BinaryConverter.calculateBinaryValue(0);
+		assertTrue("0".equals(binaryRepresentaion.toString()));
+		
+		binaryRepresentaion = BinaryConverter.calculateBinaryValue(2);
+		assertTrue("10".equals(binaryRepresentaion.toString()));
+		
+		binaryRepresentaion = BinaryConverter.calculateBinaryValue(529);
+		assertTrue("1000010001".equals(binaryRepresentaion.toString()));
+		
+		binaryRepresentaion = BinaryConverter.calculateBinaryValue(20);
+		assertTrue("10100".equals(binaryRepresentaion.toString()));
+		
+		binaryRepresentaion = BinaryConverter.calculateBinaryValue(483647);
+		assertTrue("1110110000100111111".equals(binaryRepresentaion.toString()));
+		
+		binaryRepresentaion = BinaryConverter.calculateBinaryValue(1041);
+		assertTrue("10000010001".equals(binaryRepresentaion.toString()));
+		
+		binaryRepresentaion = BinaryConverter.calculateBinaryValue(15);
+		assertTrue("1111".equals(binaryRepresentaion.toString()));
+		
+	}
+
+}
