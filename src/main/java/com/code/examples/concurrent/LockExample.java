b93584138ef949dec1fc657e6e86c162184cf0fc
erencan <erencan@netas.com.tr>
2017-02-10 09:45:34 +0300

    examples for concurrency

---
+++ src/main/java/com/code/examples/concurrent/LockExample.java
@@ -0,0 +1,74 @@
+package com.code.examples.concurrent;
+import java.util.concurrent.locks.*;
+import java.util.*;
+import java.text.*;
+
+public class LockExample {
+    public static Lock this_lock = new ReentrantLock();
+    public static void main(String[] args) throws Exception {
+    ListenerThread lt = new ListenerThread();
+        SpeakerThread st = new SpeakerThread();
+    lt.start();
+        st.start();
+    }
+    public static int message = 0;
+    public static void report(String label) {
+        Date now = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("hh:mm:ss.S");
+        String time_of_day = sdf.format(now.getTime());
+    System.out.format("%s:  %s\n", time_of_day, label);
+    }
+}
+
+class ListenerThread extends Thread {
+    public void run() {
+    Thread this_thread = Thread.currentThread();
+        while (true) {
+            LockExample.report("The Listener requests the lock.");
+            LockExample.this_lock.lock();
+            LockExample.report("The Listener acquires the lock.");
+            if (LockExample.message == 0) {
+                LockExample.report("The Speaker has sent nothing.");
+                LockExample.report("The Listener will check again later.");
+                try {
+                    this_thread.sleep(800);
+                } catch (InterruptedException ie) {
+                    // ie
+                }
+            } else {
+                LockExample.report("The Listener just received " + Integer.toString(LockExample.message) + " from the Speaker.");
+                LockExample.message = 0;
+            }
+            LockExample.report("The Listener releases the lock.");
+            LockExample.this_lock.unlock();
+        }
+    }
+}
+
+class SpeakerThread extends Thread {
+    int counter = 0;
+    public void run() {
+    Thread this_thread = Thread.currentThread();
+        while (true) {
+            while (0 != LockExample.message) {
+                try {
+                    LockExample.report("The Speaker waits for the Listener to catch up.");
+                    this_thread.sleep(30);
+                } catch (InterruptedException ie) {
+                }
+            }
+            counter++;
+            LockExample.report("The Speaker requests the lock.");
+            LockExample.this_lock.lock();
+            LockExample.report("The Speaker acquires the lock.");
+            LockExample.report("The Speaker takes a nap--a model for real work it might otherwise do.");
+            try {
+                this_thread.sleep((int) (2000 * Math.random()));
+            } catch (InterruptedException ie) {
+            }
+            LockExample.message = counter;
+            LockExample.report("The Speaker releases the lock.");
+            LockExample.this_lock.unlock();
+        }
+    }
+}
